d13047826a093462daaa9eaece7af89a7de49d2d
shaijukumar <shaijukumar1980@gmail.com>
2020-04-12 09:33:16 +0400

    section 9

---
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Item, Button, Segment, Icon } from 'semantic-ui-react';
+//import ActivityStore from '../../../app/store/activityStore';
+import { IActivity } from '../../../app/models/activity';
+import { Link } from 'react-router-dom';
+
+
+const ActivityListItem : React.FC<{activity:IActivity}> = ({activity}) => {
+
+    //const activityStore =useContext(ActivityStore);
+   // const {activitiesByDate, deleteActivity, submitting, target } = activityStore;
+
+    return (
+
+        <Segment.Group key={activity.id}>
+            <Segment>
+                <Item.Group>
+                    <Item>      
+                        <Item.Image size='tiny' circular src='/assets/user.png' />     
+                        <Item.Content>
+                            <Item.Header as='a'>{activity.title}</Item.Header>                            
+                            <Item.Description>
+                                Hosted by Bob               
+                            </Item.Description>                        
+                        </Item.Content>
+                    </Item>
+                </Item.Group>                
+            </Segment>
+
+            <Segment>
+                <Icon name='clock'/>{activity.date}
+                <Icon name='marker'/>{activity.venue}{activity.city}
+            </Segment>
+            <Segment secondary>
+                Attendees will go here
+            </Segment>
+
+            <Segment clearing>
+                <span>{activity.description}</span>
+                <Button 
+                    as={Link} to={`/activities/${activity.id}`}
+                    floated='right' 
+                    content='View' 
+                    color='blue' 
+                />
+            </Segment>
+        </Segment.Group>
+
+        
+    )
+}
+
+export default ActivityListItem
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,14 +1,18 @@
 import React, { useContext, useEffect } from 'react'
-import { Card, Image, Button } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import ActivityStore from '../../../app/store/activityStore';
 import { observer } from 'mobx-react-lite';
-import { RouteComponentProps, Link } from 'react-router-dom';
+import { RouteComponentProps} from 'react-router-dom';
 import LoadingComponent from '../../../app/layouts/LoadingComponent';
+import ActivityDetailedHeader from './ActivityDetailedHeader';
+import ActivityDetailedInfo from './ActivityDetailedInfo';
+import ActivityDetailedChat from './ActivityDetailedChat';
+import ActivityDetailedSidebar from './ActivityDetailedSidebar';
 
 interface DetailParms{
     id:string
 }
-const ActivityDetails: React.FC<RouteComponentProps<DetailParms> > = ({match, history}) => {
+const ActivityDetails: React.FC<RouteComponentProps<DetailParms> > = ({match}) => {
 
     const activityStore = useContext(ActivityStore)
     const {activity, loadActivity, loadingInitial } = activityStore;
@@ -21,27 +25,17 @@ const ActivityDetails: React.FC<RouteComponentProps<DetailParms> > = ({match, hi
     
 
     return (
-        <Card fluid>
-           <Image src={`/assets/categoryImages/${activity!.category}.jpg`} wrapped ui={false} />
-            <Card.Content>
-                <Card.Header>{activity!.title}</Card.Header>
+      <Grid>
+          <Grid.Column width='10'>
+            <ActivityDetailedHeader activity={activity}/>
+            <ActivityDetailedInfo activity={activity}/>
+            <ActivityDetailedChat/>
+          </Grid.Column>
 
-                <Card.Meta>
-                    <span className='date'>{activity!.date}</span>
-                </Card.Meta>
-
-                <Card.Description>
-                    {activity!.description}
-                </Card.Description>
-            </Card.Content>
-
-            <Card.Content extra>
-                    <Button.Group widths={2}>
-                        <Button as={Link} to={`/manage/${activity.id}`} basic color='blue' content='Edit'/>
-                        <Button onClick={ () => history.push('/activities') } basic color='blue' content='Cancel'/>
-                    </Button.Group>
-            </Card.Content>
-        </Card>
+          <Grid.Column width='6'>
+            <ActivityDetailedSidebar/>
+          </Grid.Column>
+      </Grid>
     )
 }
 
